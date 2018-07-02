f48161dc4d25d4e37d682ba5d8a80083ec0b59f5
Martin Mogusu <martinmogusu@gmail.com>
2018-07-03 01:37:18 +0300

    Added theme and welcome page

--- queues/views.py
+++ queues/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+	page_context = {}
+	return render(request, 'index.html', context=page_context)
 	return HttpResponse('Welcome to SmartQueue Prediction System')
