8a284deee0127a4bf540a7edf577ec0e084d9df3
Martin Mogusu <martinmogusu@gmail.com>
2018-07-03 16:26:24 +0300

    Added home(dashboard) page layout

--- queues/views.py
+++ queues/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	page_context = {}
+	page_context = {
+		'title': 'Today'
+	}
 	return render(request, 'index.html', context=page_context)
 	return HttpResponse('Welcome to SmartQueue Prediction System')
--- smart_queue/static/bsb/js/app.js
+++ smart_queue/static/bsb/js/app.js
@@ -1,1 +1,23 @@
-﻿
+﻿jQuery(function($){
+	// Charts js code
+	var weekly_trends_chart = function(){
+		config = {
+		    type: 'bar',
+		    data: {
+		        labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+		        datasets: [{
+		            label: "Queue Numbers",
+		            data: [140, 79, 120, 172, 58],
+		            backgroundColor: 'rgba(0, 188, 212, 0.8)'
+		        }]
+		    },
+		    options: {
+		        responsive: true,
+		        legend: false
+		    }
+		}
+		return config;
+	};
+
+    new Chart(document.getElementById("weekly-trends-chart").getContext("2d"), weekly_trends_chart());
+});
